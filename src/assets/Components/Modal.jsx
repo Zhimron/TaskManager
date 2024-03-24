6ba57e5dc6ed65d7c5b777ea7c0ddf80b1aa8c77
Zhimron <gurayshimron@gmail.com>
2024-03-24 15:36:08 +0800

    modal for login done

--- src/assets/Components/Modal.jsx
+++ src/assets/Components/Modal.jsx
@@ -1,19 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { motion } from 'framer-motion';
+import { BiSolidErrorAlt } from "react-icons/bi";
+import { HiMiniInformationCircle } from "react-icons/hi2";
 
-function Modal() {
+
+function Modal({messageBool , Info}) {
+    const [message, setMessage] = useState(messageBool);
+    useEffect(() => {
+      if (message) {
+        setMessage(true);
+        
+      } else {
+        setMessage(false);
+      }
+    }, [messageBool]);
+  
   return (
-    <div className=' flex absolute w-full  justify-center '>
-     
-      
-        <div className="modal-box">
-          <h3 className="font-bold text-lg font-text ">Hello!</h3>
-          <p className="py-4">Press ESC key or click outside to close</p>
-        </div>
-        <form method="dialog" className="modal-backdrop">
-          <button>close</button>
-        </form>
-      
-    </div>
+    <motion.div
+      className=" flex absolute w-full  justify-center scale-0"
+      initial={{ opacity: 0, scale: 0.5 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{ duration: 0.4 }}
+    >
+      <div className="modal-box">
+        {message ? (
+          <h3 className="font-bold text-lg font-text flex items-center justify-center  ">
+            {" "}
+            <HiMiniInformationCircle className="text-green-700 text-3xl" />{" "}
+            <span className="ml-1 text-3xl">Success</span>
+          </h3>
+        ) : (
+          <h3 className="font-bold text-lg font-text flex items-center justify-center    ">
+            {" "}
+            <BiSolidErrorAlt className="text-crimson text-3xl" />{" "}
+            <span className="ml-1 text-3xl"> Failed </span>
+          </h3>
+        )}
+
+        <p className="py-4 flex items-center justify-center ">{Info}</p>
+      </div>
+    </motion.div>
   );
 }
 
--- src/login/Login.jsx
+++ src/login/Login.jsx
@@ -1,6 +1,6 @@
   import React, { memo ,useState, useEffect} from "react";
   import { useForm } from 'react-hook-form';
-  import { FaUser, FaLock, FaUnlock, FaArrowRight } from "react-icons/fa";
+  import { FaUser, FaLock, FaUnlock, FaArrowRight, FaSleigh } from "react-icons/fa";
   import InputDefault from "../assets/Components/InputDefault";
   import { ButtonComp } from "../assets/Components/ButtonComp";
   import { motion } from "framer-motion";
@@ -12,90 +12,100 @@ import Modal from "../assets/Components/Modal";
 
 
   const Login = memo(() => {
-    const [showModal, setshowModal] = useState(false);
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const [loginerror,setloginError] = useState('');
-    
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [loginerror, setloginError] = useState("");
+
     const url_log = "http://127.0.0.1:8000/api/login";
     const { data: datalogin, mutate: mutateLogin } = useCustomMutation(
       url_log,
       "POST"
     );
-     const url_reg = "http://127.0.0.1:8000/api/user";
-     const { data: dataregister, mutate: mutateRegister } = useCustomMutation(
-       url_reg,
-       "POST"
-     );
+    const url_reg = "http://127.0.0.1:8000/api/user";
+    const { data: dataregister, mutate: mutateRegister } = useCustomMutation(
+      url_reg,
+      "POST"
+    );
 
-    const handleSubmitLog = (e)=>{
+    const handleSubmitLog = (e) => {
       e.preventDefault();
-      if (!username||!password) {
+      if (!username || !password) {
         setloginError("Complete all the fields");
         return;
       }
-       mutateLogin({
-         name: username,
-         password: password,
-       });
-    }
-      const [regUsername, setregUsername] = useState('');
-      const [regPassword, setregPassword] = useState('');
-      const [regEmail, setregEmail] = useState('');
-      const [regReentrypassword, setregReentryPassword] = useState('');
-      const [registererror, setRegisterError] = useState('');
+      mutateLogin({
+        name: username,
+        password: password,
+      });
+    };
+    const [regUsername, setregUsername] = useState("");
+    const [regPassword, setregPassword] = useState("");
+    const [regEmail, setregEmail] = useState("");
+    const [regReentrypassword, setregReentryPassword] = useState("");
+    const [registererror, setRegisterError] = useState("");
 
-      const handleSubmitReg = (e) => {
-        e.preventDefault();
-        if (!regUsername || !regEmail || !regPassword || !regReentrypassword) {
-          setRegisterError("Complete all the fields");
-          return;
-        }
-        if(regPassword !== regReentrypassword){
-          setRegisterError("Password not Match");
-          return;
-        }
-          mutateRegister({
-            name: regUsername,
-            email: regEmail,
-            password: regPassword,
-          });
-      };
+    const handleSubmitReg = (e) => {
+      e.preventDefault();
+      if (!regUsername || !regEmail || !regPassword || !regReentrypassword) {
+        setRegisterError("Complete all the fields");
+        return;
+      }
+      if (regPassword !== regReentrypassword) {
+        setRegisterError("Password not Match");
+        return;
+      }
+      mutateRegister({
+        name: regUsername,
+        email: regEmail,
+        password: regPassword,
+      });
+    };
 
     const [signUp, toSignUp] = useState(false);
-    
-    const triggerSignUp = () =>{
+
+    const triggerSignUp = () => {
       toSignUp(true);
       setloginError("");
     };
     const triggerLogIn = () => {
-    toSignUp(false);
-    setRegisterError("");
+      toSignUp(false);
+      setRegisterError("");
     };
     const navigate = useNavigate();
- 
-   useEffect(() => {
-   let timerId;
-     if (datalogin && datalogin.error !== "Unauthorized") {          
-      localStorage.setItem("access_token", datalogin.access_token);  
-      setshowModal(true);
-      console.log(showModal);
-      timerId = setTimeout(()=>{
-         navigate("/Userform");
-      },5000)
-     }
-     return () => {
-       clearTimeout(timerId);
-     };
-   }, [ handleSubmitLog]);
 
+    const [showModal, setShowModal] = useState(false);
+    const [isLogin, setIsLogin] = useState(false);
+    const [ismessage, setIsMessage] = useState('');
+
+    useEffect(() => {
+      let timerId;
+      if (datalogin && datalogin.error !== "Unauthorized") {
+        localStorage.setItem("access_token", datalogin.access_token);
+        setShowModal(true);
+        setIsLogin(true);
+        setIsMessage(`Login Successfully: Welcome ${username}`);
+        timerId = setTimeout(() => {
+          navigate("/Userform");
+        }, 3000);
+      } else if (datalogin && datalogin.error === "Unauthorized") {
+         setShowModal(true);
+        setIsLogin(false);
+        setIsMessage("Login Failed: Check Your Username or Password");
+        timerId = setTimeout(()=>{
+          setShowModal(false);
+        },1500)
+      }
+
+      return () => {
+        clearTimeout(timerId);
+      };
+    }, [datalogin]); // Update the dependency here to datalogin
 
     return (
       <div className="w-screen h-screen bg-royalblue flex justify-center items-center">
         {/* <Modal /> */}
         <RedirectIfLoggedIn />
-        {showModal && <Modal />}
-
+        {showModal && <Modal messageBool={isLogin} Info={ismessage} />}
         {!signUp && (
           <motion.div
             className="flex justify-center bg-ivory bg-opacity-25 w-2/6 h-2/3 rounded-md"
